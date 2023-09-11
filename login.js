02272004c3f653ea86d40235bad5a12eed8cd84f
Karl Eduard Vaina <103428011+karlviana@users.noreply.github.com>
2023-09-11 15:09:00 +0800

    mod

--- login.js
+++ login.js
@@ -2,4 +2,47 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 
-form.addEventListener('submit', e => {e.preventDefault();});
+form.addEventListener('submit', e => {e.preventDefault();
+       validateInputs();
+});
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if(usernameValue === ''){
+        setError(username, 'Please input username');
+     }
+     else {
+         setSuccess(username);
+     }
+
+    if(passwordValue === ''){
+        setError(password, 'Please input password');
+     }
+     else if (passwordValue.length < 8){
+         setError(password, 'Password must be atleast 8 characters long');
+     }
+     else {
+         setSuccess(password);
+     }
+    
+}
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.remove('success');
+    inputControl.classList.add('error');
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+}
